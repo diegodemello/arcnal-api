e4142605171a31b677a4b5c70f909b65e8bc5fb7
diegodemello <diegodemello027@gmail.com>
2025-12-11 11:32:50 -0300

    first commit

---
+++ src/test/java/br/com/arcnal/arcnal/ArcnalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcnal.arcnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcnalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
