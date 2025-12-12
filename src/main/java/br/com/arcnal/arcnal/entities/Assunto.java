ed2a7b7f6a3cf55c600016be0f6509c4132f0410
Diego de Mello <diegodemello027@gmail.com>
2025-12-12 18:08:01 -0300

    fix: adiciona os atributos e as anotações na classe Assunto.

--- src/main/java/br/com/arcnal/arcnal/entities/Assunto.java
+++ src/main/java/br/com/arcnal/arcnal/entities/Assunto.java
@@ -1,4 +1,19 @@
 package br.com.arcnal.arcnal.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "assuntos")
+@Getter
+@Setter
 public class Assunto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "id_materia")
+    private Materia materia;
+    private String nome;
 }
