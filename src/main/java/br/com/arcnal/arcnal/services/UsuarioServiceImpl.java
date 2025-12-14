3aaaf13dd7fc2feb940dd9899aff1426a7f8f5b9
Diego de Mello <diegodemello027@gmail.com>
2025-12-14 19:46:00 -0300

    refactor: altera a service de Usuário para tirar a lógica de mapeamento de DTO e entidade e passa para o Mapper.

--- src/main/java/br/com/arcnal/arcnal/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/arcnal/arcnal/services/UsuarioServiceImpl.java
@@ -5,45 +5,34 @@ import br.com.arcnal.arcnal.dtos.UsuarioReqDTO;
 import br.com.arcnal.arcnal.dtos.UsuarioRespDTO;
 import br.com.arcnal.arcnal.entities.Usuario;
 import br.com.arcnal.arcnal.entities.enums.Cargo;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.arcnal.arcnal.mapper.UsuarioMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioServiceImpl implements IUsuarioService {
 
-    @Autowired
-    UsuarioDAO dao;
+    private final UsuarioDAO usuarioDAO;
+    private final UsuarioMapper usuarioMapper;
 
     @Override
     public void cadastrarUsuario(UsuarioReqDTO dto, String enderecoIp) {
-        if(dao.existsByEmail(dto.email())){
+        if(usuarioDAO.existsByEmail(dto.email())){
             throw new RuntimeException("Esse e-mail já está em uso, faça o login.");
         }
 
-        Usuario usuario = Usuario.builder()
-                .nome(dto.nome())
-                .email(dto.email())
-                .senha(dto.senha())
-                .enderecoIp(enderecoIp)
-                .cargo(Cargo.USUARIO)
-                .banido(false)
-                .build();
-        dao.save(usuario);
+        Usuario usuario = usuarioMapper.requestToEntity(dto);
+        usuario.setEnderecoIp(enderecoIp);
+        usuario.setCargo(Cargo.USUARIO);
+        usuarioDAO.save(usuario);
     }
 
     @Override
     public List<UsuarioRespDTO> listarUsuarios() {
-        List<UsuarioRespDTO> dto = dao.findAll().stream()
-                .map(usuario -> UsuarioRespDTO.builder()
-                        .id(usuario.getId())
-                        .nome(usuario.getNome())
-                        .email(usuario.getEmail())
-                        .banido(usuario.isBanido())
-                        .cargo(usuario.getCargo())
-                        .build()
-                ).toList();
-        return dto;
+        List<UsuarioRespDTO> usuarios = usuarioMapper.entitiesToDto(usuarioDAO.findAll());
+        return usuarios;
     }
 }
