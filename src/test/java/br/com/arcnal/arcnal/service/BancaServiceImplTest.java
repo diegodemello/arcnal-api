8f18f486ba8b5a7ba14a5cfd2f53fd2ea33162ab
diegodemello <diegodemello027@gmail.com>
2025-12-16 14:59:39 -0300

    test: adiciona testes unitários para BancaServiceImpl.

---
+++ src/test/java/br/com/arcnal/arcnal/service/BancaServiceImplTest.java
@@ -0,0 +1,38 @@
+package br.com.arcnal.arcnal.service;
+
+
+import br.com.arcnal.arcnal.dao.BancaDAO;
+import br.com.arcnal.arcnal.dto.BancaRequestDTO;
+import br.com.arcnal.arcnal.exception.BancaExistenteException;
+import br.com.arcnal.arcnal.mapper.BancaMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class BancaServiceImplTest {
+
+    @InjectMocks
+    BancaServiceImpl bancaService;
+
+    @Mock
+    BancaDAO bancaDAO;
+    @Mock
+    BancaMapper bancaMapper;
+
+    @Test
+    @DisplayName("Deve lançar exceção quando tentar adicionar uma banca que já existe")
+    public void validarSeABancaJaExiste(){
+        BancaRequestDTO bancaRequest = new BancaRequestDTO("Escola XYZ");
+        Mockito.when(bancaDAO.existsByNome(bancaRequest.nome()))
+                .thenReturn(true);
+        Assertions.assertThrows(BancaExistenteException.class, () -> {
+            bancaService.adicionarBanca(bancaRequest);
+        });
+    }
+}
