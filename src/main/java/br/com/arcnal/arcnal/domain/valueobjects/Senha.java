0ce0a8e461bfcdd45ffbf611bc325be1272c0955
diegodemello <diegodemello027@gmail.com>
2025-12-31 09:18:54 -0300

    refactor: aumenta o número minimo da senha, adiciona validações de caracter especial, letra maiuscula e números.

--- src/main/java/br/com/arcnal/arcnal/domain/valueobjects/Senha.java
+++ src/main/java/br/com/arcnal/arcnal/domain/valueobjects/Senha.java
@@ -14,6 +14,9 @@ public class Senha {
     public Senha(String senha) {
         validarSePossuiEspaco(senha);
         validarTamanho(senha);
+        validarSePossuiCaracterEspecial(senha);
+        validarSePossuiLetraMaiuscula(senha);
+        validarSePossuiNumero(senha);
         this.senha = senha;
     }
 
@@ -24,8 +27,26 @@ public class Senha {
     }
 
     private void validarTamanho(String senha){
-        if(senha.length() < 6 || senha.length() > 20){
-            throw new SenhaInvalidaException("A senha deve ter entre 6 e 20 caracteres.");
+        if(senha.length() < 8 || senha.length() > 20){
+            throw new SenhaInvalidaException("A senha deve ter entre 8 e 20 caracteres.");
+        }
+    }
+
+    private void validarSePossuiCaracterEspecial(String senha){
+        if(!senha.matches(".*[^a-zA-Z0-9].*")){
+            throw new SenhaInvalidaException("A senha deve conter pelo menos um caractere especial.");
+        }
+    }
+
+    private void validarSePossuiLetraMaiuscula(String senha){
+        if(!senha.matches(".*[A-Z].*")){
+            throw new SenhaInvalidaException("A senha deve conter pelo menos uma letra maiúscula.");
+        }
+    }
+
+    private void validarSePossuiNumero(String senha){
+        if(!senha.matches(".*\\d.*")){
+            throw new SenhaInvalidaException("A senha deve conter pelo menos um número.");
         }
     }
 }
