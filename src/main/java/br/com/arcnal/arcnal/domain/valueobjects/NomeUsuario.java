d00923ca888ecff354d5149530e9024ca010bdcf
diegodemello <diegodemello027@gmail.com>
2026-02-19 16:15:12 -0300

    refactor: implementa nos Value Objects Email, Nome e Senha do Usuário a interface Serializable para serializar e deserializar.

--- src/main/java/br/com/arcnal/arcnal/domain/valueobjects/NomeUsuario.java
+++ src/main/java/br/com/arcnal/arcnal/domain/valueobjects/NomeUsuario.java
@@ -5,10 +5,13 @@ import jakarta.persistence.Embeddable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Embeddable
 @NoArgsConstructor
-public class NomeUsuario {
+public class NomeUsuario implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String nome;
 
     public NomeUsuario(String nome) {
