74facdd80be0aece6332c96133f31584d1902aa7
Diego de Mello <diegodemello027@gmail.com>
2026-03-01 09:15:07 -0300

    feat: cria o método na repository para buscar Estatística por usuario, matéria e assunto.

--- src/main/java/br/com/arcnal/arcnal/domain/repositories/EstatisticaUsuarioRepository.java
+++ src/main/java/br/com/arcnal/arcnal/domain/repositories/EstatisticaUsuarioRepository.java
@@ -1,7 +1,18 @@
 package br.com.arcnal.arcnal.domain.repositories;
 
+import br.com.arcnal.arcnal.domain.entities.Assunto;
 import br.com.arcnal.arcnal.domain.entities.EstatisticaUsuario;
+import br.com.arcnal.arcnal.domain.entities.Materia;
+import br.com.arcnal.arcnal.domain.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface EstatisticaUsuarioRepository extends JpaRepository<EstatisticaUsuario, Long> {
+    Optional<EstatisticaUsuario> findByUsuarioAndMateriaAndAssunto(
+            Usuario usuario,
+            Materia materia,
+            Assunto assunto
+    );
 }
